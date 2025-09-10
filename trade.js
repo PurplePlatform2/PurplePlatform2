2b14c70d735338b2203dad090c7b994cfa184a8e
PurplePlatform2 <ovegies9ja@gmail.com>
2025-09-10 22:30:10 +0100

    Update trade.js

--- trade.js
+++ trade.js
@@ -12,7 +12,7 @@ if (typeof window !== "undefined" && window.WebSocket) {
 
 /* === CONFIG === */
 const APP_ID = 1089; // Your Deriv App ID
-const token = "GrDCl7fo5axufb2"; // 🔐 Replace with your real token
+const token = "tUgDTQ6ZclOuNBl"; // 🔐 Replace with your real token
 const stake = 2;
 const symbol = "stpRNG";
 const multiplier = 750;
