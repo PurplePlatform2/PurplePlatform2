508951c61f230e5be774cfdaf778a6c66472ce69
PurplePlatform2 <ovegies9ja@gmail.com>
2025-10-16 21:08:40 +0100

    Back to demo

--- trade.js
+++ trade.js
@@ -8,7 +8,7 @@
  ****************************************************/
 
 const APP_ID = 1089;
-const TOKEN = "1Ej5Kd5yebuR6LN";
+const TOKEN = "tUgDTQ6ZclOuNBl";
 const SYMBOL = "stpRNG";
 const BASE_STAKE = 1;
 const DURATION =59;           // trade lasts 60 seconds
